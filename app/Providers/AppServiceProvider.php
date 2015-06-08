567fe2decb7cd5331ad7901a6a38e32e6287e72a
matsuo <matsuo@h2system.jp>
2015-06-08 10:35:28 +0900

    created: 新規作成。日本語化、IDEHelper、Generators-Extend、DebugBarを追加

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,42 @@
+<?php namespace App\Providers;
+
+use Barryvdh\Debugbar\ServiceProvider as DebugBarServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+use Illuminate\Support\ServiceProvider;
+use Laracasts\Generators\GeneratorsServiceProvider;
+
+class AppServiceProvider extends ServiceProvider {
+
+    /**
+     * アプリケーションサービスの初期化処理
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * アプリケーションサービスの登録
+     *
+     * このサービスプロバイダーは、アプリケーションの様々な
+     * コンテナ結合を行うのに最適です。ご覧の通り、"Registrar"実装を
+     * ここで登録しています。皆さんの結合も追加できますよ！
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'Illuminate\Contracts\Auth\Registrar',
+            'App\Services\Registrar'
+        );
+        if ($this->app->environment('local')) {
+            $this->app->register(IdeHelperServiceProvider::class);
+            $this->app->register(GeneratorsServiceProvider::class);
+            $this->app->register(DebugBarServiceProvider::class);
+        }
+    }
+
+}
